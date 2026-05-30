ba7fe3e34c7645f5daefdc4be625180ca01e82bf
ary <ary@hyperbeam.sh>
2026-05-29 21:15:01 -0700

    add grappling hook chain

--- objects/gameplay/player/hook.gd
+++ objects/gameplay/player/hook.gd
@@ -9,6 +9,10 @@ var grappled: bool = false
 func _ready() -> void:
 	$hook_ray.target_position.y = max_distance
 
+func _process(delta: float) -> void:
+	$chain.visible = grappled
+	do_chain_graphics()
+
 func _physics_process(delta: float) -> void:
 	$hook_ray.look_at(get_global_mouse_position())
 	$hook_ray.rotation += 3 * PI/2
@@ -31,3 +35,10 @@ func apply_grapple():
 
 func release_grapple():
 	grappled = false
+	
+func do_chain_graphics():
+	var tip_loc = to_local($tip.global_position)
+	$chain.rotation = deg_to_rad(180) + self.position.angle_to_point(tip_loc) - deg_to_rad(90)
+	$tip.rotation = self.position.angle_to_point(tip_loc) - deg_to_rad(90)
+	$chain.position = tip_loc
+	$chain.region_rect.size.y = tip_loc.length()
